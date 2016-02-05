ae04aadf0f2f5da12bfd7e0df0dfbc3338a8b75f
Nibbles <siliconrob@siliconheaven.net>
2016-02-05 09:01:21 -0800

    Fix image/png http responses
    Also update to ServiceStack 4.0.52 and .NET 4.5.2

--- src/TileFixer.ServiceInterface/TileLayer.cs
+++ src/TileFixer.ServiceInterface/TileLayer.cs
@@ -6,26 +6,23 @@ namespace Tile.ServiceInterface
 {
   public class TileLayer : Service
   {
-    [AddHeader(ContentType = "image/png")]
     public object Get(MetaTile request)
     {
-      return TileFactory.MetaTile(request);
+      return new HttpResult(TileFactory.MetaTile(request), MimeTypes.ImagePng);
     }
 
-    [AddHeader(ContentType = "image/png")]
     public object Get(SpectrumMetaTile request)
     {
-      return TileFactory.SpectrumMetaTile(request);
+      return new HttpResult(TileFactory.SpectrumMetaTile(request), MimeTypes.ImagePng);
     }
 
-    [AddHeader(ContentType = "image/png")]
     public object Get(GetTile request)
     {
       var log = this.Log();
       var cacheKey = request.CacheKey();
       log.DebugFormat("Cache Id: {0}", cacheKey);
       var result = Cache.ToResultUsingCache(cacheKey, TileFactory.RawTile(request));
-      return result.Image;
+      return new HttpResult(result.Image, MimeTypes.ImagePng);
     }
 
     public object Get(GetTileBounds request)
